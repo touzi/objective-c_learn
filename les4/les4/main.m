cb4764e40ac9ebce71d549f42d0ebe9d8c1e4f53
touzi <policetouzi@163.com>
2015-04-17 17:44:56 +0800

    continue例子

--- les4/les4/main.m
+++ les4/les4/main.m
@@ -11,12 +11,22 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // break跳出的使用
+        //直接跳出循环体
         for (int i=0; i<100; i++) {
             if (i == 8) {
                 break;
             }
         }
         NSLog(@"Hello, World!");
+        
+        //continue跳出本次循环之后，继续循环
+        for (int j=0; j<100; j++) {
+            if (j%2==0) {
+                NSLog(@"我是continue结果：%d",j);
+                continue;
+            }
+            NSLog(@"yizhi zai xunhuan : %d",j);
+        }
     }
     return 0;
 }
